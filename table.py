d5bf3fa3b9ae17e47eacef96fed39646fc4cd17c
Aditya <123227683+Aditya-0918@users.noreply.github.com>
2023-09-18 14:28:56 +0530

    Add files via upload
    Debug

---
+++ table.py
@@ -0,0 +1,9 @@
+import mysql.connector as sql
+conn=sql.connect(host='localhost',user='root',passwd='manager'database='bank')
+ifconn.is_connected():
+    print('connected succesfully')
+cur=conn.cursor()
+cur.execute('create table customer_details(acct_noint
+primary key,acct_namevarchar(25) ,phone_nobigint(25)
+check(phone_no>11),address varchar(25),cr_amt
+float )')

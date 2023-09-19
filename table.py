87da2b149fdb1d334d872e201ed35858e2ffbeeb
Aditya <123227683+Aditya-0918@users.noreply.github.com>
2023-09-19 12:46:36 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,9 +1,9 @@
 import mysql.connector as sql
-conn=sql.connect(host='localhost',user='root',passwd='manager'database='bank')
-ifconn.is_connected():
+conn=sql.connect(host='localhost',user='root',passwd='manager',database='bank')
+if conn.is_connected():
     print('connected succesfully')
 cur=conn.cursor()
-cur.execute('create table customer_details(acct_noint
+cur.execute('create table customer_details(acct_noint)
 primary key,acct_namevarchar(25) ,phone_nobigint(25)
 check(phone_no>11),address varchar(25),cr_amt
 float )')
